8490b0f4d6142e49a568edd483117ad9f850919e
Pierre BULGARE <pierre.bulgare@gmail.com>
2025-03-08 21:20:07 +0100

    Optimisation des variables

--- server.py
+++ server.py
@@ -50,44 +50,46 @@ def purchasePlaces():
     PLACE_LIMIT = 12
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
+    places_required = int(request.form['places'])
+    competition_places = int(competition['numberOfPlaces'])
+    club_points = int(club['points'])
 
     # If the competition does not have a 'clubs' key, create one
     if not competition.get('clubs'):
         competition['clubs'] = {}
 
     # Check if the competition is already fully booked
-    if int(competition['numberOfPlaces']) <= 0:
+    if competition_places <= 0:
         flash('Sorry, this competition is already fully booked')
 
     # Check if the number of places requested is more than the limit
-    elif int(request.form['places']) > PLACE_LIMIT:
+    elif places_required > PLACE_LIMIT:
         flash('Sorry, you can only purchase up to 12 places')
 
     # Check if the number of places requested is more than the number of points
-    elif int(request.form['places']) > int(club['points']):
+    elif places_required > club_points:
         flash('Sorry, you do not have enough points to purchase this many places')
 
     # Check if the number of places requested is more than the number of places left in the competition
-    elif int(request.form['places']) > int(competition['numberOfPlaces']):
+    elif places_required > competition_places:
         flash('Sorry, there are not enough places left in this competition based on your request')
 
     # If all the checks pass, update the number of points and places left
     else:
         # If the club is not in the competition, add it
         if not competition['clubs'].get(club['name']):
-            competition['clubs'][club['name']] = {'places': 0 }
+            competition['clubs'][club['name']] = { 'places': 0 }
 
         # Check if the club will reach the limit of 12 places after booking
-        if competition['clubs'][club['name']]['places'] + placesRequired > PLACE_LIMIT:
+        if competition['clubs'][club['name']]['places'] + places_required > PLACE_LIMIT:
             flash('Sorry, only 12 places are allowed per club')
         else:
             # Update the number of places left for the competition
-            competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+            competition['numberOfPlaces'] = competition_places - places_required
             # Update the number of points left for the club
-            club['points'] = int(club['points'])-placesRequired
+            club['points'] = club_points - places_required
             # Update the number of places booked by the club for the competition
-            competition['clubs'][club['name']]['places'] += placesRequired
+            competition['clubs'][club['name']]['places'] += places_required
             flash('Great-booking complete!')
 
     return render_template('welcome.html', club=club, competitions=competitions)
