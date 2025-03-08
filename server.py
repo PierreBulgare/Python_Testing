da1768a1933ebcf2a6a1193ab955b8713ad39d47
Pierre BULGARE <pierre.bulgare@gmail.com>
2025-03-09 00:01:08 +0100

    Ajout d'une page pour l'erreur 404

--- server.py
+++ server.py
@@ -105,3 +105,7 @@ def purchasePlaces():
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
