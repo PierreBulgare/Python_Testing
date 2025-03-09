f24b466e826a54e284c84886d8e9335058b06c2d
Pierre BULGARE <pierre.bulgare@gmail.com>
2025-03-09 02:49:28 +0100

    Ajout d'une restriction pour empêcher la réservation de place par un autre club que celui qui est connecté

--- server.py
+++ server.py
@@ -89,14 +89,22 @@ def showSummaryGet():
 @app.route('/book/<competition>/<club>')
 @login_required
 def book(competition,club):
-    print(club,competition)
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+    # Get the club details using the email address of the current user
+    connected_club = next((club for club in clubs if club['email'] == current_user.id), None)
+    # Check if the club name in the URL matches the club name of the current user
+    if connected_club and club == connected_club['name']:
+        foundCompetition = [c for c in competitions if c['name'] == competition][0]
+        # If the club and competition are found, render the booking page
+        if foundCompetition:
+            return render_template('booking.html',club=connected_club,competition=foundCompetition)
+        # If the club or competition is not found, redirect to the showSummary page
+        else:
+            flash("Something went wrong-please try again")
+            return render_template('welcome.html', club=club, competitions=competitions)
+    # If the club name in the URL does not match the club name of the current user, redirect to the showSummary page
     else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        flash("You are not allowed to book for another club")
+        return redirect(url_for('showSummaryGet'))
 
 
 @app.route('/purchasePlaces',methods=['POST'])
@@ -105,53 +113,59 @@ def purchasePlaces():
     PLACE_LIMIT = 12
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
-    places_required = int(request.form['places'])
-    competition_places = int(competition['numberOfPlaces'])
-    club_points = int(club['points'])
+    # Get the club details using the email address of the current user
+    if club['email'] == current_user.id:
+        places_required = int(request.form['places'])
+        competition_places = int(competition['numberOfPlaces'])
+        club_points = int(club['points'])
 
-    # If the competition does not have a 'clubs' key, create one
-    if not competition.get('clubs'):
-        competition['clubs'] = {}
+        # If the competition does not have a 'clubs' key, create one
+        if not competition.get('clubs'):
+            competition['clubs'] = {}
 
-    # Check if the number of places requested is less than or equal to 0
-    if places_required <= 0:
-        flash('Sorry, you must purchase at least 1 place')
+        # Check if the number of places requested is less than or equal to 0
+        if places_required <= 0:
+            flash('Sorry, you must purchase at least 1 place')
 
-    # Check if the competition is already fully booked
-    elif competition_places <= 0:
-        flash('Sorry, this competition is already fully booked')
+        # Check if the competition is already fully booked
+        elif competition_places <= 0:
+            flash('Sorry, this competition is already fully booked')
 
-    # Check if the number of places requested is more than the number of places left in the competition
-    elif places_required > competition_places:
-        flash('Sorry, there are not enough places left in this competition based on your request')
+        # Check if the number of places requested is more than the number of places left in the competition
+        elif places_required > competition_places:
+            flash('Sorry, there are not enough places left in this competition based on your request')
 
-    # Check if the number of places requested is more than the limit
-    elif places_required > PLACE_LIMIT:
-        flash('Sorry, you can only purchase up to 12 places')
+        # Check if the number of places requested is more than the limit
+        elif places_required > PLACE_LIMIT:
+            flash('Sorry, you can only purchase up to 12 places')
 
-    # Check if the number of places requested is more than the number of points
-    elif places_required > club_points:
-        flash('Sorry, you do not have enough points to purchase this many places')
+        # Check if the number of places requested is more than the number of points
+        elif places_required > club_points:
+            flash('Sorry, you do not have enough points to purchase this many places')
 
-    # If all the checks pass, update the number of points and places left
-    else:
-        # If the club is not in the competition, add it
-        if not competition['clubs'].get(club['name']):
-            competition['clubs'][club['name']] = { 'places': 0 }
-
-        # Check if the club will reach the limit of 12 places after booking
-        if competition['clubs'][club['name']]['places'] + places_required > PLACE_LIMIT:
-            flash('Sorry, only 12 places are allowed per club')
+        # If all the checks pass, update the number of points and places left
         else:
-            # Update the number of places left for the competition
-            competition['numberOfPlaces'] = competition_places - places_required
-            # Update the number of points left for the club
-            club['points'] = club_points - places_required
-            # Update the number of places booked by the club for the competition
-            competition['clubs'][club['name']]['places'] += places_required
-            flash('Great-booking complete!')
+            # If the club is not in the competition, add it
+            if not competition['clubs'].get(club['name']):
+                competition['clubs'][club['name']] = { 'places': 0 }
 
-    return render_template('welcome.html', club=club, competitions=competitions)
+            # Check if the club will reach the limit of 12 places after booking
+            if competition['clubs'][club['name']]['places'] + places_required > PLACE_LIMIT:
+                flash('Sorry, only 12 places are allowed per club')
+            else:
+                # Update the number of places left for the competition
+                competition['numberOfPlaces'] = competition_places - places_required
+                # Update the number of points left for the club
+                club['points'] = club_points - places_required
+                # Update the number of places booked by the club for the competition
+                competition['clubs'][club['name']]['places'] += places_required
+                flash('Great-booking complete!')
+                
+        return render_template('welcome.html', club=club, competitions=competitions)
+    else:
+        flash("You are not allowed to book for another club")
+        return redirect(url_for('showSummaryGet'))
+
 
 
 # TODO: Add route for points display
